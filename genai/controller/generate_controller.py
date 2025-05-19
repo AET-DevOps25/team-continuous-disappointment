07f30f3ee05129a0f9cc264f08a4410bf18a404c
Ege Dogu Kaya <117287716+edkaya@users.noreply.github.com>
2025-05-19 22:55:55 +0200

    Seperate ci/cd yaml from server yaml

--- genai/controller/generate_controller.py
+++ genai/controller/generate_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 
 generate_bp = Blueprint('generate', __name__)
 
+
 @generate_bp.route('/api/generate', methods=['POST'])
 def generate():
     return jsonify({'output': 'Hello World!'})
