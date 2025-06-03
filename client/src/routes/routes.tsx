61ba5488c6e84f974a2ffb77f82155c05758d37d
Mehmed Esad AKÇAM <m.esadakcam@hotmail.com>
2025-06-03 21:35:04 +0200

    Add login page

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -2,7 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import { PrivateRoute } from "./PrivateRoute";
 import ChatPage from "../pages/ChatPage";
+import LoginPage from "../pages/LoginPage";
 export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
   {
     path: "/",
     element: <Layout />,
