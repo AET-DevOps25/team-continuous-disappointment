71f14be06cb9f1cef76007da4a5615ff99e5d4f7
Ege Dogu Kaya <117287716+edkaya@users.noreply.github.com>
2025-06-22 03:16:34 +0200

    trigger tests

--- genai/routes/routes.py
+++ genai/routes/routes.py
@@ -182,3 +182,4 @@ async def generate(request: Request):
         duration = perf_counter() - start_time
         generation_duration.observe(duration)
         logger.info("Response generation duration: %.2f seconds", duration)
+
