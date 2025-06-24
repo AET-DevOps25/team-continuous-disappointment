d1d030f3e342357dbb685b683e6730aeaa384ac1
Mehmed Esad AKÇAM <m.esadakcam@hotmail.com>
2025-06-24 22:09:25 +0200

    Add logging

--- server/user/src/main/java/com/continiousdisappointment/user/controller/UserController.java
+++ server/user/src/main/java/com/continiousdisappointment/user/controller/UserController.java
@@ -3,11 +3,15 @@ package com.continiousdisappointment.user.controller;
 import com.continiousdisappointment.user.domain.User;
 import com.continiousdisappointment.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping("info")
 @RequiredArgsConstructor
@@ -15,7 +19,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public User getUserInfo(@RequestHeader("Authorization") String authorization) {
-        return userService.getUserInfo(authorization);
+    public ResponseEntity<User> getUserInfo(@RequestHeader("Authorization") String authorization) {
+        try {
+            return ResponseEntity.ok(userService.getUserInfo(authorization));
+        } catch (Exception e) {
+            log.warn("Error while getting user info", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
--- server/user/src/main/java/com/continiousdisappointment/user/service/UserService.java
+++ server/user/src/main/java/com/continiousdisappointment/user/service/UserService.java
@@ -1,11 +1,13 @@
 package com.continiousdisappointment.user.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.continiousdisappointment.user.domain.User;
 
+@Log4j2
 @Service
 public class UserService {
 
@@ -14,6 +16,7 @@ public class UserService {
     public User getUserInfo(String authorization) {
         String url = "https://gitlab.lrz.de/api/v4/user";
         if (authorization == null) {
+            log.warn("No access token found in security context");
             throw new IllegalStateException("No access token found in security context");
         }
         HttpHeaders headers = new HttpHeaders();
